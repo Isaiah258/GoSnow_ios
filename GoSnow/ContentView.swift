baeccbbd402fad95ae60db8311f87494684ebe12
Isaiah258 <hideonbush0061@gmail.com>
2024-06-18 16:41:06 +0800

    Initial Commit

---
+++ GoSnow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoSnow
+//
+//  Created by federico Liu on 2024/6/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoSnow/GoSnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoSnowApp.swift
+//  GoSnow
+//
+//  Created by federico Liu on 2024/6/18.
+//
+
+import SwiftUI
+
+@main
+struct GoSnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
